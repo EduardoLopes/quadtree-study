3ef923cdc5b17800ea51a63c706bc233857df19c
Eduardo Lopes <EduardoLopesDS@gmail.com>
2015-01-26 01:18:54 -0300

    Add get center methods to AABB.js

--- js/AABB.js
+++ js/AABB.js
@@ -12,8 +12,16 @@
       if(this.x < other.x + other.width && this.y < other.y + other.height
         && this.x + this.width > other.x && this.y + this.height > other.y){
         return true;
-      }
+      };
       return false;
+    };
+
+    this.getCenterX = function(){
+      return ( this.getX() + this.getWidth() ) / 2;
+    }
+
+    this.getCenterY = function(){
+      return ( this.getY() + this.getHeight() ) / 2;
     }
 
   });
