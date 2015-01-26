7193f24b342c469f91f89e71216875593803c677
Eduardo Lopes <EduardoLopesDS@gmail.com>
2015-01-26 00:00:27 -0300

    Add point class and extend.js

---
+++ js/point.js
@@ -0,0 +1,32 @@
+(function(global, undefined){
+
+  'use strict';
+
+  var Point = Class.extend(function(){
+
+    this.constructor = function( x, y ){
+      this.setX(x);
+      this.setY(x);
+    };
+
+    this.getX = function() {
+      return this.x;
+    };
+
+    this.getY = function() {
+      return this.y;
+    };
+
+    this.setX = function(x) {
+      this.x = x;
+    };
+
+    this.setY = function(y){
+      this.y = y;
+    };
+
+  });
+
+  global.Point = Point;
+
+})(window);
