6670c8ff52826d61197c3e9f1587ba1ea90269f4
Eduardo Lopes <EduardoLopesDS@gmail.com>
2015-01-26 03:40:13 -0300

    Add initial quadtree class working with test

--- js/main.js
+++ js/main.js
@@ -2,4 +2,53 @@
 
   'use strict';
 
+  var canvas = document.getElementById('quadtree'),
+      ctx = canvas.getContext('2d');
+
+      canvas.width = 500;
+      canvas.height = 500;
+
+  var q = new QuadTree( new AABB(0,0,500,500) )
+
+  q.insert( new AABB(10,10,20,20) );
+  q.insert( new AABB(10,300,20,20) );
+  q.insert( new AABB(300,300,20,20) );
+  q.insert( new AABB(200,300,20,20) );
+  q.insert( new AABB(300,300,20,20) );
+  q.insert( new AABB(200,300,20,20) );
+  q.insert( new AABB(300,300,20,20) );
+  q.insert( new AABB(200,300,20,20) );
+  q.insert( new AABB(200,300,20,20) );
+  q.insert( new AABB(200,300,20,20) );
+  q.insert( new AABB(300,300,20,20) );
+  q.insert( new AABB(200,300,20,20) );
+
+
+  console.log(q);
+
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0,0,500,500);
+
+  function drawTree(node){
+
+    ctx.beginPath();
+    ctx.rect( node.boundary.x, node.boundary.y, node.boundary.width, node.boundary.height );
+    ctx.closePath();
+    ctx.lineWidth = 2;
+    ctx.stroke();
+
+    ctx.fillStyle = '#000';
+    ctx.fillRect( node.boundary.getCenterX() - 2.5, node.boundary.getCenterY() - 2.5, 5, 5);
+
+
+    for (var i =  0; i < node.nodes.length; i++) {
+
+      drawTree(node.nodes[i]);
+
+    };
+
+  }
+
+  drawTree(q);
+
 })(window);
--- js/quadtree.js
+++ js/quadtree.js
@@ -2,6 +2,87 @@
 
   'use strict';
 
+  var QuadTree = Class.extend(function(){
+
+    var MAX_ENTITIES = 2;
+    this.entities = [];
+    this.numEntities = 0;
+    this.nodes = [];
+
+    this.constructor = function( AABB ){
+      this.setBoundary( AABB );
+    };
+
+    this.setBoundary = function( AABB ){
+      this.boundary = AABB;
+    };
+
+    this.insert = function( entity ){
+      if( entity.intersectsAABB( this.boundary ) ){
+        if(this.numEntities < MAX_ENTITIES){
+          this.entities[this.numEntities] = entity;
+          this.numEntities++;
+        } else {
+          this.insertToChild( entity );
+        }
+      }
+    };
+
+    this.insertToChildNode = function( entity, x, y, width, height, index ){
+
+      if(this.nodes[index] == null){
+        this.nodes[index] = new QuadTree( new AABB( x, y, width, height ) );
+      }
+
+      if( entity.intersectRect( x, y, width, height ) ){
+        this.nodes[index].insert(entity);
+      }
+
+    };
+
+    this.insertToChild = function( entity ){
+
+      this.insertToChildNode(
+        entity,
+        this.boundary.getX(),
+        this.boundary.getY(),
+        this.boundary.getHalfXDimention(),
+        this.boundary.getHalfYDimention(),
+        0
+      );
+
+      this.insertToChildNode(
+        entity,
+        this.boundary.getCenterX(),
+        this.boundary.getY(),
+        this.boundary.getHalfXDimention(),
+        this.boundary.getHalfYDimention(),
+        1
+      );
+
+      this.insertToChildNode(
+        entity,
+        this.boundary.getX(),
+        this.boundary.getCenterY(),
+        this.boundary.getHalfXDimention(),
+        this.boundary.getHalfYDimention(),
+        2
+      );
+
+      this.insertToChildNode(
+        entity,
+        this.boundary.getCenterX(),
+        this.boundary.getCenterY(),
+        this.boundary.getHalfXDimention(),
+        this.boundary.getHalfYDimention(),
+        3
+      );
+
+    };
+
+  });
+
+  global.QuadTree = QuadTree;
 
 
 })(window);
