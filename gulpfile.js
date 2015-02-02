0acd0b25aa8c25cd824213028c13a1a3bb171cbf
Eduardo Lopes <EduardoLopesDS@gmail.com>
2015-02-02 17:21:34 -0300

    Remove useless requires from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
-var mocha = require('gulp-mocha');
-var jasmine = require('gulp-jasmine');
 
 gulp.task('watch', function() {
 
